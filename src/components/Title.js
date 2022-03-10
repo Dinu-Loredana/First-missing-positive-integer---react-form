e6fac1af82dffbd268b8d67783b3044b54b097c4
Loredana Dinu <dinuloredana.m@gmail.com>
2022-03-10 19:27:35 +0000

    add paragraph

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,10 @@
 const Title = () => {
 
-    return <h2 style={{textAlign:'center', color: 'blue', fontSize:'1em'}}>First Missing Positive Integer</h2>
+    return <>
+        <p>Can you guess the number?</p>
+        <p>Let's see!</p>
+        <h2 style={{textAlign:'center', color: 'blue', fontSize:'1em'}}>First Missing Positive Integer</h2>
+    </>
 }
 
 
