76cdbd7587358044c4f4ed6633a5d376c37c74bf
Loredana Dinu <dinuloredana.m@gmail.com>
2022-03-11 15:28:15 +0000

    add more paragraphs to tes1.js

--- src/components/tes1.js
+++ src/components/tes1.js
@@ -1,3 +1,12 @@
 const tes1 = () => {
-    return <p>tes1 here</p>
+    return
+    <>
+    <p>tes1 here</p>
+    <p>tes2 here</p>
+    <p>tes3 here</p>
+    <p>tes4 here</p>
+    <p>tes5 here</p>
+    <p>tes5 here</p>
+    <p>tes5 here</p>
+    </>
 }
